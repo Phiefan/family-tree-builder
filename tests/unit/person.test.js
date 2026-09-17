203bb11ec4508571c3a0327fe9ff4a47b87fe5e8
Phiefan <miatho13@outlook.com>
2026-09-17 03:29:52 +0200

    test: add Person personal information tests

---
+++ tests/unit/person.test.js
@@ -0,0 +1,38 @@
+
+describe('Person', () => {
+    let person;
+
+    beforeEach(()=>{
+        person = new Person('John', 'Doe');
+    });
+
+    describe('Personal Information', () => {
+        test('stores firstName', () => {
+            expect(person.firstName).toBe('John');
+        });
+
+        test('stores last name', () => {
+            expect(person.lastName).toBe('Doe');
+        });
+
+        describe('fullName', () => {
+            test('combines firstName and lastName', ()=>{
+                expect(person.fullName).toBe('John Doe');
+            });
+
+            test('reflects changes to firstName and lastName', ()=>{
+                person.firstName='Richard';
+                person.lastName= 'Roe';
+
+                expect(person.fullName).toBe('Richard Roe');
+            });
+
+            test('is read-only', ()=>{
+                person.fullName = 'Richard Roes';
+                
+                expect(person.fullName).toBe('John Doe');
+            });
+        });
+    });
+
+});
